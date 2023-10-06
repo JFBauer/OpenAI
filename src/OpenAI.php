38084a686d43ac84a32cb2a28d65f00c735c8716
Jasper Bauer <jasper@jasperbauer.nl>
2023-10-06 23:34:16 +0200

    First Version of the API

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -2,6 +2,8 @@
 
 namespace JFBauer\OpenAI;
 
+use GuzzleHttp\Client;
+
 class OpenAI
 {
     protected string|null $apiKey;
@@ -17,5 +19,26 @@ class OpenAI
         if (empty($this->apiKey)) {
             throw new \Exception('The OPENAI_API_KEY environment variable is not set.');
         }
+    }
+
+    public function callOpenAI(Request $request)
+    {
+        $client = new Client();
+
+        $response = $client->post('https://api.openai.com/v1/engines/davinci-codex/completions', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->apiKey,
+                'Content-Type' => 'application/json',
+                'User-Agent' => 'Laravel-OpenAI-Client'
+            ],
+            'json' => [
+                'prompt' => "Translate the following English text to French: 'Hello, how are you?'",
+                'max_tokens' => 150
+            ]
+        ]);
+
+        $response = json_decode($response->getBody(), true);
+
+        return $response;
     }
 }
